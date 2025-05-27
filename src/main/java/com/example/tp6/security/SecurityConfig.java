9e169eb10967c64af24898e5d79b0dc342f2e018
Youssef Kitabrhi <kitabrhi.youssef.1@gmail.com>
2025-05-27 21:42:40 +0100

    v2

--- src/main/java/com/example/tp6/security/SecurityConfig.java
+++ src/main/java/com/example/tp6/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
